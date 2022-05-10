1a994f7929527ea11a66f8b9311cfecc7c76161b
sprislly <99052001+sprislly@users.noreply.github.com>
2022-05-10 14:06:47 -0700

    Comments

--- Set Assignment Sally/src/Main.java
+++ Set Assignment Sally/src/Main.java
@@ -8,27 +8,37 @@ public class Main {
 
     public String character;
 
-
     public static void main(String[] args) throws FileNotFoundException {
-        int counter = 0;
         Scanner scanner = new Scanner(new File("test.txt"));
+        /*
+        Find Unique Words.
+        @param requires String.
+        @return the unique words in order.
+        */
         LinkedHashSet<String> unique = new LinkedHashSet<>();
         while(scanner.hasNext()) {
+            /*
+            [^a-zA-Z0-9] specifies all characters except numbers and letter.
+                For example, - , . and ; are all replaced.
+             */
             unique.add(scanner.next().replaceAll("[^a-zA-Z0-9]", ""));
 
         }
 
+        int counter = 0;
         for (String s : unique) {
             System.out.println(s);
             counter ++;
         }
 
         System.out.println("\nThere are " + counter + " unique words in the Iliad.");
-
-
     }
 
-
+    /*
+    Only Add Unique Strings.
+    @param requires String.
+    @return true or false depending on if the String is already in the Set.
+    */
     @Override
     public boolean equals(Object obj) {
         if(obj instanceof String temp){
@@ -37,9 +47,5 @@ public class Main {
         return false;
     }
 
-    @Override
-    public int hashCode() {
-        return character.hashCode();
-    }
 
 }
